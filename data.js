3666fa21c920b878a35b7f2bc5caf4baa86a2476
michalbpraha-dotcom <michalbpraha@gmail.com>
2026-06-15 23:22:01 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,234 @@
+const katalog = {
 
+OBUV:{
+
+Adidas:[
+"Bad Bunny x Gazelle Indoor San Juan",
+"Campus",
+"Gazelle Classic",
+"Gazelle Gucci",
+"SL 72",
+"Spezial"
+],
+
+Balenciaga:[
+"Balenciaga 2.0",
+"Balenciaga Track 3"
+],
+
+Converse:[
+"Converse All Star"
+],
+
+Crocs:[
+"Crocs Bape"
+],
+
+"DC Shoes":[
+"DC Shoes"
+],
+
+Dior:[
+"Dior B23 Oblique"
+],
+
+"Dolce & Gabbana":[
+"Portofino"
+],
+
+"Karl Lagerfeld":[
+"Kapri Iconic"
+],
+
+Lacoste:[
+"Powercourt"
+],
+
+Loewe:[
+"Loewe x On Running Cloud Solo"
+],
+
+"Nike":[
+"Air Force 1 Supreme",
+"Air Jordan 4",
+"Air Jordan 4 Kaws",
+"Air Jordan 4 Retro Pizza",
+"Air Jordan 4 Retro Undefeated",
+"Air Jordan 4 Retro OG SP Nigel Sylvester Brick by Brick",
+"Air Jordan 4RM x Nigel Sylvester Pro Green",
+"Air Max 1",
+"Air Max 270",
+"Jordan 1 Mid French Blue",
+"Kobe 8 Protro",
+"Nike Air Zoom",
+"Nike Levi's",
+"Nike Mind 001",
+"Nike OFF White kotníkové",
+"Nike V5 RNR",
+"OFF-WHITE Vulc Low",
+"Stussy x Air Zoom Spiridon Caged 2 Fossil"
+],
+
+"New Balance":[
+"New Balance 1000",
+"New Balance 574 Legacy Stone Island",
+"New Balance 9060",
+"New Balance 998"
+],
+
+"On Running":[
+"ON CLOUD 6"
+],
+
+Puma:[
+"Puma Suede XL"
+],
+
+Salomon:[
+"MM6 Maison Margiela x Salomon X-ALP",
+"Salomon XT-6 Copen Blue",
+"Salomon Trekking"
+],
+
+Timberland:[
+"Timberland",
+"Timberland Bape",
+"Timberland Outdoor"
+],
+
+Vans:[
+"Vans Knu Skool"
+]
+
+},
+
+TEXTIL:{
+
+Bundy:[
+"Arc'teryx",
+"BAPE Coca-Cola",
+"Nike ACG Skull Peak",
+"Helly Hansen Odin Mountain",
+"Lacoste",
+"Napapijri Casual Pullover Hooded Fleece",
+"Nike Paris Saint-Germain",
+"Ralph Lauren",
+"The North Face Kaws"
+],
+
+Mikiny:[
+"BAPE",
+"Carhartt",
+"Essentials",
+"Gant",
+"GAP",
+"Jil Sander",
+"Jordan Supreme",
+"Ralph Lauren",
+"Tommy Hilfiger"
+],
+
+"Tepláky / Soupravy":[
+"Eric Emanuel",
+"Essentials",
+"Lacoste",
+"Nike Paris Saint-Germain",
+"Nike Sportswear Tech Fleece",
+"Nike Tepláky",
+"Nocta",
+"Patagonia",
+"Polo Ralph Lauren",
+"Tommy Hilfiger"
+],
+
+Trička:[
+"Balenciaga",
+"BAPE",
+"Corteiz World Rules",
+"FI",
+"Gant",
+"Jil Sander",
+"Karl Lagerfeld",
+"Lacoste Polo Classic",
+"Lacoste Polo",
+"Maison Margiela",
+"Moncler Polo",
+"Nike Nocta",
+"Ralph Lauren",
+"Tommy Hilfiger"
+],
+
+"Kraťasy / Kalhoty":[
+"Carhartt",
+"Essentials",
+"Lacoste",
+"Lee",
+"Nike Nocta",
+"Stussy"
+],
+
+"Trenky / Ponožky":[
+"Balenciaga Trenky",
+"Calvin Klein Trenky",
+"Calvin Klein Ponožky",
+"Lacoste Trenky",
+"NBA Ponožky",
+"Nike Ponožky",
+"OFF White Trenky",
+"Ralph Lauren Trenky",
+"Supreme Trenky",
+"Tommy Hilfiger Trenky"
+],
+
+Plavky:[
+"Dsquared2",
+"Ralph Lauren",
+"Tommy Hilfiger"
+]
+
+},
+
+OSTATNI:{
+
+"Bačkory / Pantofle":[
+"Balenciaga Pantofle",
+"Nike Bačkory",
+"Ralph Lauren",
+"Yeezy Slide"
+],
+
+"Batohy / Tašky":[
+"Adidas",
+"BAPE",
+"Carhartt",
+"Essentials Batohy",
+"Essentials Taška",
+"Nike Max Air Backpack",
+"Sprayground",
+"Supreme",
+"The North Face"
+],
+
+Pásky:[
+"Carhartt",
+"Dickies"
+],
+
+"Skateboardy / Prkna":[
+"BAPE",
+"Kaws",
+"Tom and Jerry",
+"Vans"
+],
+
+Brýle:[
+"Ray-Ban"
+],
+
+Termosky:[
+"Stanley"
+]
+
+}
+
+};
