e9f16cbc11ae05cb99042f1d715beae54069c475
michalbpraha-dotcom <michalbpraha@gmail.com>
2026-06-19 05:59:12 +0200

    Update data.js

--- data.js
+++ data.js
@@ -73,7 +73,7 @@ Loewe:[
 "New Balance 1000",
 "New Balance 574 Legacy Stone Island",
 "New Balance 9060",
-"New Balance 997",
+"New Balance 997"
 ],
 
 "On Running":[
@@ -160,7 +160,7 @@ Trička:[
 "Kraťasy - Kalhoty":[
 "Carhartt",
 "Essentials",
-"Lacoste",
+"Lacoste"
 ],
 
 "Trenky - Ponožky":[
